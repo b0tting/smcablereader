bd8ff32057e19853e72fde2bd35c7a6dee1b845a
b0tting <malkav@dds.nl>
2016-03-21 18:45:10 +0000

    Added optional URL for raw output

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ A gloriously simple Python script that serves your current energy consumption in
 This script boots a Flask webserver on port 80. Requirements are thus Flask and a library for reading a serial port. 
 - pip install pyserial Flask
 
-I had some minor issues with reading from the /dev/ttyUSB0 by default, solved by a "chmod a+rw /dev/ttyUSB0" 
+I had some minor issues with reading from the /dev/ttyUSB0 by default, solved by a "chmod a+rw /dev/ttyUSB0"
+
+## Usage
+Start. Then either open 0.0.0.0:82 for a JSON view of the current usage and last 10 measurements. Or open /raw to see the last full output. Note that this terminates with a !, so if the last line does not start with an exclamation mark your meter was still busy writing it's output when you requested the page. 
+
+
+
+ 
--- smcablereader.py
+++ smcablereader.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 from flask import Flask, jsonify
 import sys
 import time
@@ -9,7 +8,7 @@ import re
 app = Flask(__name__)
 
 ## Based on http://gejanssen.com/howto/Slimme-meter-uitlezen/
-## First, run pip install pyserial
+## First, run pip install pyserial Flask
 ## Also, I had to chmod /dev/ttyUSB0
 
 
@@ -30,13 +29,16 @@ current = 0
 current_return = 0
 history = [0] * 20
 
+last = []
 def read_meter():
-    global current
     global history
+    global current
+    global current_return
 
     current_pattern = re.compile('1-0:1.7.0\(([0-9]+\.[0-9]+)\*')
     current_return_pattern = re.compile('1-0:2.7.0\(([0-9]+\.[0-9]+)\*')
     ser.open()
+    empty_first = False;
     while True:
         try:
             p1_raw = ser.readline()
@@ -49,17 +51,27 @@ def read_meter():
                 return
             except:
                 pass
-            
-        match = current_pattern.match(str(p1_raw))
+
+        current_line = str(p1_raw)
+
+        match = current_pattern.match(current_line)
         if(match and match.group(1)):
             current = int(float(match.group(1)) * 1000)
-	    history = history[1:]
+            history = history[1:]
             history.append(current)
         else:
             ## Teruggegeven waarde zal vaak 0 zijn - deze wordt al ingehouden op de afgenomen stroom
-            match = current_return_pattern.match(str(p1_raw))
-            if(match and match.group(1)):
+            match = current_return_pattern.match(current_line)
+            if match and match.group(1):
                 current_return = int(float(match.group(1)) * 1000)
+
+        if current_line[:1] == "!":
+            empty_first = True
+        elif empty_first:
+                last[:] = []
+                empty_first = False
+
+        last.append(current_line)
 
     ## Unreacheable, but for future reference: close the serial connection here
     try:
@@ -68,8 +80,13 @@ def read_meter():
         pass
 
 @app.route('/')
-def show_current(): 
+def show_current():
     return jsonify({"current_usage_mw":current,"current_return_mw":current_return, "history_usage_mw":history })
+
+@app.route('/raw')
+def show_raw():
+    return ''.join(last)
+
 
 thread = Thread(target = read_meter)
 thread.daemon=True
